adfbc7b9405a3af41c38df8ec880fc12c65f431d
Ryoji-sakamoto <rskmt1218.ee@gmail.com>
2021-01-16 07:06:13 +0900

    rename dir name

---
+++ myAnswer/1.js
@@ -0,0 +1,4 @@
+const a = { a: 'a' };
+const b = { b: 'b' };
+const c = Object.assign({}, a, b);
+//console.log(c);//
---
+++ myAnswer/2.js
@@ -0,0 +1,2 @@
+const arry = ['aa', 'bb', 'cc', 'dd', 'ee', 'ff', 'gg'];
+const newArry = arry.slice(3, -1);
